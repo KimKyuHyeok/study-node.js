1f06bca5b72a4c330bfdc2b9fdc6793b57142b97
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-02-27 05:24:04 +0900

    기본적인 틀 작성

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const ejs = require('ejs');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: false}));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index'); // ./views/index.ejs
 })
+
+app.get('/profile', (req, res) => {
+    res.render('profile');
+})
+
+app.get('/map', (req, res) => {
+    res.render('map');
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
+
+app.post('/contactProc', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const memo = req.body.memo;
+
+    var a = `${name} ${email} ${memo}`
+
+    res.send(a);
+})
+
 
 
 app.listen(port, () => {
